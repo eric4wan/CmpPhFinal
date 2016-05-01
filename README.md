9372b114cae22435853946a2728f8a5440f937d1
Eric Wan <eric4wan@gmail.com>
2016-05-01 15:48:44 -0400

    working

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # CmpPhFinal
 
 
+Written on Python 2.7.11
 
-Note: OSX seems to require the following commands
+
+As far as we can tell, OSX seems to require the following commands:
 
 brew install boost --with-python
 brew install boost-python
@@ -10,3 +12,7 @@ sudo chown -R $USER:admin /usr/local/include
 brew link boost
 sudo easy_install cmake
 sudo pip install dlib
+
+Make sure to extract shape_predictor_68_face_landmarks.dat.zip into the project directory!
+
+Run using 'python main.py'
--- main.py
+++ main.py
@@ -1,203 +1,204 @@
-import sys
-import os
-import dlib
+#!/usr/bin/python
+
+# Copyright (c) 2015 Matthew Earl
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+#     The above copyright notice and this permission notice shall be included
+#     in all copies or substantial portions of the Software.
+#
+#     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+#     OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#     MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+#     NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+#     DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+#     OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+#     USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+
+Please consult the README on build instructions
+
+Small sections of this code were borrowed (legally/ethically/rightfully) from
+online resources. A Copyright statement from one author has been included above,
+as per their request.
+
+The pipeline was completely written from scratch, including the webcam frame/output
+manipulation. The transformation matrix was painstakingly derived from wiki but then
+StackOverflow provided a *cough* somewhat insignificant* inspirational resource.
+
+Other sources were used as slight references, but this code is ultimately *ours* -
+we wrote it and it's ours and we wrote it. Enjoy!
+
+Eric Wan
+Amanda Grace Wall
+"""
+
 import cv2
-import numpy
+import dlib
+import numpy as np
+import sys
+import glob
+import shutil
 
-# https://github.com/burningion/faceswap/blob/master/swapfacesvideo.py
-# http://www.kpkaiser.com/programming/the-mostly-newbies-guide-to-automatically-swapping-faces-in-video/
+# dlib has a 62 'hardcoded' facial landmarks to look for
+# the necessary ones are enumerated here
+MOUTH = list(range(48, 61))
+RIGHT_BROW = list(range(17, 22))
+LEFT_BROW = list(range(22, 27))
+RIGHT_EYE = list(range(36, 42))
+LEFT_EYE = list(range(42, 48))
+NOSE = list(range(27, 35))
 
-PREDICTOR_PATH = "/Users/ericwan/Downloads/shape_predictor_68_face_landmarks.dat"
-SCALE_FACTOR = 1
-FEATHER_AMOUNT = 11
+# points used to line up the images.
+ALIGN_POINTS = (LEFT_BROW + RIGHT_EYE + LEFT_EYE +
+                               RIGHT_BROW + NOSE + MOUTH)
 
-FACE_POINTS = list(range(17, 68))
-MOUTH_POINTS = list(range(48, 61))
-RIGHT_BROW_POINTS = list(range(17, 22))
-LEFT_BROW_POINTS = list(range(22, 27))
-RIGHT_EYE_POINTS = list(range(36, 42))
-LEFT_EYE_POINTS = list(range(42, 48))
-NOSE_POINTS = list(range(27, 35))
-JAW_POINTS = list(range(0, 17))
+# will need the convex hull of each of these elemnts to overlay on face 1 from face 2
+OVERLAY_POINTS = []
 
-# Points used to line up the images.
-ALIGN_POINTS = (LEFT_BROW_POINTS + RIGHT_EYE_POINTS + LEFT_EYE_POINTS +
-                               RIGHT_BROW_POINTS + NOSE_POINTS + MOUTH_POINTS)
+# basically a switch function
+def excl(arg):
+    switch = {
+        'A': [LEFT_EYE + RIGHT_EYE + LEFT_BROW + RIGHT_BROW,
+        NOSE + MOUTH,],
+        'E': [LEFT_EYE + RIGHT_EYE,],
+        'N': [NOSE,],
+        'M': [MOUTH,]
+    }
+    return switch.get(arg, "INVALID INPUT")
 
-# Points from the second image to overlay on the first. The convex hull of each
-# element will be overlaid.
-OVERLAY_POINTS = [
-    LEFT_EYE_POINTS + RIGHT_EYE_POINTS + LEFT_BROW_POINTS + RIGHT_BROW_POINTS,
-    NOSE_POINTS + MOUTH_POINTS,
-]
+# start of keyboard input to control the faceswap parameters
+bo = raw_input("Hello! Welcome to live FaceSwap. Would you like to begin or review the options? ('B' or 'O')\n")
+if (bo == "O"):
+    ex = raw_input("Which features would you like to swap? Please type only one letter and hit enter to add it to the list.\n\
+        All features = 'A'\n Eyes = 'E'\n Nose = 'N'\n Mouth = 'M'\n")
+    OVERLAY_POINTS = excl(ex)
+    print len(OVERLAY_POINTS)
 
-# Amount of blur to use during colour correction, as a fraction of the
-# pupillary distance.
-COLOUR_CORRECT_BLUR_FRAC = 0.6
+if (bo == "B"):
+    OVERLAY_POINTS = [LEFT_EYE + RIGHT_EYE + LEFT_BROW + RIGHT_BROW, NOSE + MOUTH,]
+
+if (((bo != "B") and (bo != "O")) or len(OVERLAY_POINTS) == 0):
+    print "QUITTING"
+    sys.exit(0)
 
 detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor(PREDICTOR_PATH)
-def draw_convex_hull(im, points, color):
-    points = cv2.convexHull(points)
-    cv2.fillConvexPoly(im, points, color=color)
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
-
-def warp_im(im, M, dshape):
-    output_im = numpy.zeros(dshape, dtype=im.dtype)
-    cv2.warpAffine(im,
-                   M[:2],
-                   (dshape[1], dshape[0]),
-                   dst=output_im,
-                   borderMode=cv2.BORDER_TRANSPARENT,
-                   flags=cv2.WARP_INVERSE_MAP)
-    return output_im
-
-def get_face_mask(im, landmarks):
-    im = numpy.zeros(im.shape[:2], dtype=numpy.float64)
+def face_outline(im, landmarks):
+    im = np.zeros(im.shape[:2], dtype = np.float64)
 
     for group in OVERLAY_POINTS:
-        draw_convex_hull(im,
-                         landmarks[group],
-                         color=1)
+        points = cv2.convexHull(landmarks[group])
+        cv2.fillConvexPoly(im, landmarks[group], color = 1)
 
-    im = numpy.array([im, im, im]).transpose((1, 2, 0))
+    im = np.array([im, im, im]).transpose((1, 2, 0))
 
-    im = (cv2.GaussianBlur(im, (FEATHER_AMOUNT, FEATHER_AMOUNT), 0) > 0) * 1.0
-    im = cv2.GaussianBlur(im, (FEATHER_AMOUNT, FEATHER_AMOUNT), 0)
+    im = (cv2.GaussianBlur(im, (11, 11), 0) > 0) * 1.0
+    im = cv2.GaussianBlur(im, (11, 11), 0)
 
     return im
 
-def faceSwap():
-    print("Analyzing your photos...")
-    if (findNumFaces() < 2):
-        print("Whoops! Looks like we couldn't find more than one face to swap. Please try again!")
-        sys.exit(0)
-    print("Starting faceswap...")
-    out_list = []
-    # add result to out_list
-
-
-
-    writeOutput(out_list)
-    return True
-
-def transformation_from_points(points1, points2):
-    """
-    Return an affine transformation [s * R | T] such that:
-        sum ||s*R*p1,i + T - p2,i||^2
-    is minimized.
-    """
-    # Solve the procrustes problem by subtracting centroids, scaling by the
-    # standard deviation, and then using the SVD to calculate the rotation. See
-    # the following for more details:
-    #   https://en.wikipedia.org/wiki/Orthogonal_Procrustes_problem
-
-    points1 = points1.astype(numpy.float64)
-    points2 = points2.astype(numpy.float64)
-
-    c1 = numpy.mean(points1, axis=0)
-    c2 = numpy.mean(points2, axis=0)
+def transformation_matrix(points1, points2):
+    # computing the affine transofmration [s * R | T] to minimize
+    #   67
+    #  SIGMA || s * R * points1 + T - points2 ||^2
+    #   0
+    # reference: https://en.wikipedia.org/wiki/Orthogonal_Procrustes_problem
+    points1 = points1.astype(np.float64)
+    points2 = points2.astype(np.float64)
+    # subtract centroids
+    c1 = np.mean(points1, axis=0)
+    c2 = np.mean(points2, axis=0)
     points1 -= c1
     points2 -= c2
-
-    s1 = numpy.std(points1)
-    s2 = numpy.std(points2)
+    # scale by standard deviation
+    s1 = np.std(points1)
+    s2 = np.std(points2)
     points1 /= s1
     points2 /= s2
-
-    U, S, Vt = numpy.linalg.svd(points1.T * points2)
-
-    # The R we seek is in fact the transpose of the one given by U * Vt. This
-    # is because the above formulation assumes the matrix goes on the right
-    # (with row vectors) where as our solution requires the matrix to be on the
-    # left (with column vectors).
+    # use SVD to get rotation
+    U, S, Vt = np.linalg.svd(points1.T * points2)
+    # actually need the matrix on the left
     R = (U * Vt).T
+    outVal = np.vstack([np.hstack(((s2 / s1) * R, c2.T - (s2 / s1) * R * c1.T)), np.matrix([0., 0., 1.])])
+    return outVal
 
-    return numpy.vstack([numpy.hstack(((s2 / s1) * R,
-                                       c2.T - (s2 / s1) * R * c1.T)),
-                         numpy.matrix([0., 0., 1.])])
+def warp_im(im, M, dshape):
+    # applies the affine transformation
+    output_im = np.zeros(dshape, dtype = im.dtype)
+    cv2.warpAffine(im, M[:2], (dshape[1], dshape[0]), dst = output_im, borderMode = cv2.BORDER_TRANSPARENT, flags = cv2.WARP_INVERSE_MAP)
+    return output_im
 
-def test(im):
-    rects = detector(im, 1)
-    print("@@@@@@")
-    print(len(rects))
-    im1, landmarks1 = (im, numpy.matrix([[p.x, p.y] for p in predictor(im, rects[0]).parts()])) # first detected face
-    im2, landmarks2 = (im, numpy.matrix([[p.x, p.y] for p in predictor(im, rects[1]).parts()])) # second detected face
+def color_correction(im1, im2, landmarks1):
+    blur_amount = 0.6 * np.linalg.norm(np.mean(landmarks1[LEFT_EYE], axis = 0) - np.mean(landmarks1[RIGHT_EYE], axis = 0))
+    blur_amount = int(blur_amount)
+    if blur_amount % 2 == 0:
+        blur_amount += 1
+    im1_blur = cv2.GaussianBlur(im1, (blur_amount, blur_amount), 0)
+    im2_blur = cv2.GaussianBlur(im2, (blur_amount, blur_amount), 0)
+    # avoid type conversion and div by zero errors
+    im2_blur += (128 * (im2_blur <= 1)).astype(np.uint8)
+    out_val = (im2.astype(np.float64) * im1_blur.astype(np.float64) / im2_blur.astype(np.float64))
+    return out_val
 
-    M = transformation_from_points(landmarks1[ALIGN_POINTS], # First transformation
-                                   landmarks2[ALIGN_POINTS])
+webcam = cv2.VideoCapture(0)
+counter = 0
+fsaved = False
 
-    M1 = transformation_from_points(landmarks2[ALIGN_POINTS], # Second transformation
-                                   landmarks1[ALIGN_POINTS])
-
-    mask = get_face_mask(im2, landmarks2) # First mask
-    mask1 = get_face_mask(im1, landmarks1) # Second mask
-
-    warped_mask = warp_im(mask, M, im1.shape) # First warp
-    warped_mask1 = warp_im(mask1, M1, im2.shape) # Second warp
-
-    combined_mask = numpy.max([get_face_mask(im1, landmarks1), warped_mask],
-                              axis=0)
-    combined_mask1 = numpy.max([get_face_mask(im2, landmarks2), warped_mask1],
-                              axis=0)
-
-    warped_corrected_im2 = correct_colours(im1, warped_im2, landmarks1)
-    warped_corrected_im3 = correct_colours(im2, warped_im3, landmarks2)
-
-    output_im = im1 * (1.0 - combined_mask) + warped_corrected_im2 * combined_mask # apply first mask
-    output_im = output_im * (1.0 - combined_mask1) + warped_corrected_im3 * combined_mask1 # apply second face mask
-
-    cv2.imwrite('output.jpg', output_im)
-
-def filters():
-    print("Analyzing your photos...")
-
-
-def findNumFaces():
-    # sourcefolder = os.path.abspath(os.path.join(os.curdir, 'source'))
-    # outfolder = os.path.abspath(os.path.join(os.curdir, 'out'))
-    # if not os.path.exists(outfolder):
-    #     os.mkdir(outfolder)
-    #
-    # exts = ['.bmp', '.pbm', '.pgm', '.ppm', '.sr', '.ras', '.jpeg', '.jpg',
-    #         '.jpe', '.jp2', '.tiff', '.tif', '.png']
-    # numFaces = 0
-    # img_list = []
-    # for dirA in os.listdir(sourcefolder):
-    #     img_list = []
-    #     filenames = sorted(os.listdir(os.path.join(sourcefolder, dirA)))
-    #
-    #     for filename in filenames:
-    #         name, ext = os.path.splitext(filename)
-    #         if ext in exts:
-    #             img_list.append(cv2.imread(os.path.join(sourcefolder, video_dir,
-    #                                                     filename)))
-    # # for img in img_list:
-    #     # set numFaces
-
-    return 2
-
-
-def writeOutput(out_list):
-    print "writing output to {}".format(os.path.join(outfolder, video_dir))
-    if not os.path.exists(os.path.join(outfolder, video_dir)):
-        os.mkdir(os.path.join(outfolder, video_dir))
-
-    for idx, image in enumerate(out_list):
-        cv2.imwrite(os.path.join(outfolder,video_dir,'frame{0:04d}.png'.format(idx)), image)
-
-if __name__ == "__main__":
-    # faceOrFilter = input("Hello! Would you like to try faceSwap or filter? ")
-    image = cv2.imread("source/fs.jpeg", cv2.IMREAD_GRAYSCALE)
-    test(image)
-    feature = -1
-    # -1 = not valid
-    # 0 = FaceSwap
-    # 1 = filter
-    if (faceOrFilter == "FaceSwap"):
-        faceSwap()
-    elif (faceOrFilter == "Filter"):
-        filters()
+while True:
+    # avoid lag by waiting
+    # counter += 1
+    # if (not (counter % 5 == 0)):
+    #     continue
+    ret, frame = webcam.read()
+    # detect faces from current webcam frame
+    faces = detector(frame, 1)
+    if len(faces) < 2:
+        print "need more faces!!!"
+        continue
     else:
-        print("That was not valid input!")
-        sys.exit(0)
+        # because both faces show up in same frame, ordering is important for xy math
+        if faces[0].left() < faces[1].left():
+            im1, landmarks1 = (frame, np.matrix([[p.x, p.y] for p in predictor(frame, faces[0]).parts()]))
+            im2, landmarks2 = (frame, np.matrix([[p.x, p.y] for p in predictor(frame, faces[1]).parts()]))
+        else:
+            im1, landmarks1 = (frame, np.matrix([[p.x, p.y] for p in predictor(frame, faces[1]).parts()]))
+            im2, landmarks2 = (frame, np.matrix([[p.x, p.y] for p in predictor(frame, faces[0]).parts()]))
+
+        # get transformation matrices
+        M = transformation_matrix(landmarks1[ALIGN_POINTS], landmarks2[ALIGN_POINTS])
+        M1 = transformation_matrix(landmarks2[ALIGN_POINTS], landmarks1[ALIGN_POINTS])
+        # get actual chunk of face (if swapping all features, should look like a 'T')
+        mask = face_outline(im2, landmarks2)
+        mask1 = face_outline(im1, landmarks1)
+        # applying the transformation to the masks
+        warped_mask = warp_im(mask, M, im1.shape)
+        warped_mask1 = warp_im(mask1, M1, im2.shape)
+        # combining
+        combined_mask = np.max([face_outline(im1, landmarks1), warped_mask], axis = 0)
+        combined_mask1 = np.max([face_outline(im2, landmarks2), warped_mask1], axis = 0)
+        warped_im2 = warp_im(im2, M, im1.shape)
+        warped_im3 = warp_im(im1, M1, im2.shape)
+        # blending skin colors
+        warped_corrected_im2 = color_correction(im1, warped_im2, landmarks1)
+        warped_corrected_im3 = color_correction(im2, warped_im3, landmarks2)
+        # finishing off transformation math
+        output_im = im1 * (1.0 - combined_mask) + warped_corrected_im2 * combined_mask
+        output_im = output_im * (1.0 - combined_mask1) + warped_corrected_im3 * combined_mask1
+        # format for cv2.imshow() - if you want to use cv2.imwrite(), skip this line
+        output_im =  np.array(output_im, dtype = float) / float(255)
+        cv2.imshow('frame', output_im)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+webcam.release()
+cv2.destroyAllWindows()
+print "Bye - thanks for trying out FaceSwap!"
